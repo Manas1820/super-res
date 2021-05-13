11481e042738f5df8512c96cec3aac8324ff8099
Manas1820 <manasgupta1820@gmail.com>
2021-05-13 17:48:46 +0530

    ⚡ DB connected

--- db.sql
+++ db.sql
@@ -1,9 +1,19 @@
+-- \c to get into the database
+
+
 CREATE TABLE restaurants(
     id BIGSERIAL NOT NULL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     location VARCHAR(255) NOT NULL,
     price_range INT NOT NULL check(price_range >= 1 and price_range <= 5)
-)
+);
 
+SELECT * FROM restaurants;
+SELECT * FROM restaurants WHERE id=1;
 
-INSERT INTO restaurants(id,name,location,price_range) VALUES(5537,'Dominoes','Hell',34)
+INSERT INTO restaurants(name,location,price_range) VALUES('Dominoes','Hell',34);
+INSERT INTO restaurants(name,location,price_range) VALUES('Pizza Hut','Cheese',4) returning *;
+
+UPDATE restaurants SET name='Kullu',location='Manali',price_range=3 WHERE id=2 returning *;
+
+DELETE FROM restaurants WHERE id=1;
